eb574e4d5e1764c4d859c7f45b6c4b87b1b5d5c8
Naveen1612-svg <naveenak129@gmail.com>
2022-09-08 00:37:34 -0700

    kumar's code

--- src/test/java/org/git/Gitday1.java
+++ src/test/java/org/git/Gitday1.java
@@ -1,5 +1,11 @@
 package org.git;
 
 public class Gitday1 {
+	
+	public static void main(String[] args) {
+	System.out.println("test1");
+	System.out.println("test4");
+	System.out.println("test5");
+	}
 
 }
